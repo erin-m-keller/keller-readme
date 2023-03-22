e55d2ce3625eb281d65678e438fc5b0d94808fd0
Erin Keller <aestheticartist@gmail.com>
2023-03-22 12:47:35 -0400

    Adding function to generate the README file

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer');
+const readmeMarkdown = require('./utils/generateMarkdown.js');
 
 const questions = () => {
     return inquirer.prompt([
         {
+            type: 'input',
             name: 'title',
             message: 'What would you like the title to be for your application?',
             validate: validateInput
         },
         {
+            type: 'input',
             name: 'description',
             message: 'What description would you like to include?',
             validate: validateInput
         },
         {
+            type: 'input',
             name: 'installation',
             message: 'What installation instructions would you like to include?',
             validate: validateInput
         },
         {
+            type: 'input',
             name: 'usage',
             message: 'What usage instructions would you like to include?',
             validate: validateInput
         },
         {
+            type: 'input',
             name: 'license',
             message: 'What is the name of the license you wish to use?',
             validate: validateInput
         },        
         {
+            type: 'input',
             name: 'contributing',
             message: 'Please add all contributors by their github username (seperated by a comma):',
             validate: validateInput
         },
         {
+            type: 'input',
             name: 'questionsEmail',
             message: 'Please add your email for the questions section of the README:',
             validate: validateInput
         },
         {
+            type: 'input',
             name: 'questionsGithub',
             message: 'Please add your github username for the questions section of the README:',
             validate: validateInput
@@ -50,8 +59,7 @@ const validateInput = async (input) => {
     if (input) {
         return true;
     } else {
-        console.log('You must enter the information.');
-        return false;
+        return 'You must enter the information.';
     }
 };
 
@@ -61,8 +69,7 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
     questions().then(answers => {
-        //return generatePage(answers);
-        console.log("answers: " + answers);
+        return readmeMarkdown(answers);
     }).then(data => {
         //return writeFile(data);
         console.log("data: " + data);
