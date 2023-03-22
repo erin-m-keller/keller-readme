7a62757e43b47b60f2be4097d3528a97d8c1768a
Erin Keller <aestheticartist@gmail.com>
2023-03-22 12:33:23 -0400

    Added all questions that will be prompted to the user

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-// TODO: Include packages needed for this application
+const fs = require('fs'); 
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            name: 'title',
+            message: 'What would you like the title to be for your application?',
+        },
+        {
+            name: 'description',
+            message: 'What description would you like to include?',
+        },
+        {
+            name: 'installation',
+            message: 'What installation instructions would you like to include?',
+        },
+        {
+            name: 'usage',
+            message: 'What usage instructions would you like to include?',
+        },
+        {
+            name: 'license',
+            message: 'What is the name of the license you wish to use?',
+        },
+        {
+            name: 'contributing',
+            message: 'Please add all contributors by their github username (seperated by a comma):',
+        },
+        {
+            name: 'questionsEmail',
+            message: 'Please add your email for the questions section of the README:',
+        },
+        {
+            name: 'questionsGithub',
+            message: 'Please add your github username for the questions section of the README:',
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
