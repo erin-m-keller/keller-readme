a5777de6e2b33a49bd248afa3f615790c2de63a6
Erin Keller <aestheticartist@gmail.com>
2023-03-22 12:39:26 -0400

    Added validateInput function to check for empty string, also updated init function to start prompting the questions

--- index.js
+++ index.js
@@ -6,43 +6,70 @@ const questions = () => {
         {
             name: 'title',
             message: 'What would you like the title to be for your application?',
+            validate: validateInput
         },
         {
             name: 'description',
             message: 'What description would you like to include?',
+            validate: validateInput
         },
         {
             name: 'installation',
             message: 'What installation instructions would you like to include?',
+            validate: validateInput
         },
         {
             name: 'usage',
             message: 'What usage instructions would you like to include?',
+            validate: validateInput
         },
         {
             name: 'license',
             message: 'What is the name of the license you wish to use?',
+            validate: validateInput
         },
         {
             name: 'contributing',
             message: 'Please add all contributors by their github username (seperated by a comma):',
+            validate: validateInput
         },
         {
             name: 'questionsEmail',
             message: 'Please add your email for the questions section of the README:',
+            validate: validateInput
         },
         {
             name: 'questionsGithub',
             message: 'Please add your github username for the questions section of the README:',
+            validate: validateInput
         }
     ])
+};
+
+const validateInput = async (input) => {
+    if (input) {
+        return true;
+    } else {
+        console.log('You must enter the information.');
+        return false;
+    }
 };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions().then(answers => {
+        //return generatePage(answers);
+        console.log("answers: " + answers);
+    }).then(data => {
+        //return writeFile(data);
+        console.log("data: " + data);
+    }).catch(err => {
+        console.log("err: " + err)
+    });
+}
 
 // Function call to initialize app
 init();
