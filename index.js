4b2ac37184b389c5ea9f7c113e896c5976d6adac
Erin Keller <aestheticartist@gmail.com>
2023-03-24 11:29:07 -0400

    Added comments to the index.js file

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const fs = require('fs'); 
-const inquirer = require('inquirer');
-const readmeMarkdown = require('./utils/generateMarkdown.js');
+// initialize variables
+const fs = require('fs'),
+      inquirer = require('inquirer'),
+      readmeMarkdown = require('./utils/generateMarkdown.js');
 
+// Create the prompts
 const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'title',
             message: 'What would you like the title to be for your application?',
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         },
         {
             type: 'input',
             name: 'description',
             message: 'What description would you like to include?',
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         },
         {
             type: 'input',
             name: 'installation',
             message: 'What installation instructions would you like to include?',
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         },
         {
             type: 'input',
             name: 'usage',
             message: 'What usage instructions would you like to include?',
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         },
         {
             type: 'list',
@@ -34,7 +36,7 @@ const questions = () => {
             message: 'What is the name of the license you wish to use?',
             choices: ["MIT","GPL","CC"],
             default: "MIT",
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         }, 
         {
             type: 'list',
@@ -42,77 +44,117 @@ const questions = () => {
             message: 'What color badge would you like?',
             choices: ["red","orange","yellow","green","blue","purple","black","white"],
             default: "blue",
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         },       
         {
             type: 'input',
             name: 'contributing',
             message: 'Please add all contributors by their github username (seperated by a comma):',
-            validate: validateContributors
+            validate: validateContributors // validate the text is a comma delimited string
         },       
         {
             type: 'input',
             name: 'tests',
             message: 'Please add any tests for the application:',
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         },
         {
             type: 'input',
             name: 'questionsEmail',
             message: 'Please add your email for the questions section of the README:',
-            validate: validateEmail
+            validate: validateEmail // validate the email is valid
         },
         {
             type: 'input',
             name: 'questionsGithub',
             message: 'Please add your github username for the questions section of the README:',
-            validate: validateInput
+            validate: validateInput // validate the input is not empty
         }
     ])
 };
 
+/**
+ * @validateInput
+ * Ensures the input is not empty,
+ * returns a message to the user if
+ * it is
+ */
 const validateInput = async (input) => {
+    // if input has data, return true
     if (input) {
         return true;
-    } else {
-        return 'You must enter the information.';
-    }
+    } 
+    // else display a message to the user
+    return 'You must enter the information.';
 };
 
+/**
+ * @validateContributors
+ * Ensures the contributers string is
+ * comma delimited 
+ */
 const validateContributors = async (input) => {
+    // initialize variables
     let contributorRegex = /^[a-zA-Z0-9_-]+(?:\s*,\s*[a-zA-Z0-9_-]+)*$/;
+    // if the string matches the regex pattern, return true
     if (contributorRegex.test(input)) {
       return true;
     }
+    // else display a message to the user
     return 'You must enter the usernames, separated by a comma.';
 };
 
+/**
+ * @validateEmail
+ * Ensures a valid email has
+ * been entered
+ */
 const validateEmail = async (input) => {
+    // initialize variables
     let emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    // if the string matches the regex pattern, return true
     if (emailRegex.test(input)) {
       return true;
     }
+    // else display a message to the user
     return 'You must enter a valid email address.';
 };
 
+/**
+ * @validateEmail
+ * Writes the README data to a file
+ */
 function writeToFile(fileName, data) {
+    // write data to file
     fs.writeFile(fileName, data, (err) => {
+        // if error, show error in console log
         if (err) {
             console.error(err);
-        } else {
+        } 
+        // console log a success message
+        else {
             console.log('"GENERATED-README.md" file generated successfully! You can find your file in the root folder.');
         }
     });
 }
 
+/**
+ * @init
+ * Runs on app load
+ */
 function init() {
+    // get the data from the prompts
     questions().then(answers => {
+        // pass the answers to the markdown file to format the content
         return readmeMarkdown(answers);
     }).then(data => {
+        // pass the returned data to the @writeToFile function
         return writeToFile("GENERATED-README.md",data);
     }).catch(err => {
+        // console log the error
         console.log("Error: " + err)
     });
 }
 
+// initialize the application
 init();
