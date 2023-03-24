8dccdd08eb82a9452163158a15ab824a448072b7
Erin Keller <aestheticartist@gmail.com>
2023-03-24 11:37:50 -0400

    Added comments to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,29 @@
+/**
+ * @renderLicenseBadge
+ * Returns a string with the license,
+ * badge color, and link
+ */
 function renderLicenseBadge(userInput) {
+  // initialize variables
   let appLicense = userInput.license,
       licenseColor = userInput.badgeColor;
+  // if no input, return empty string
   if (!appLicense) {
     return "";
-  } else {
+  } 
+  // format the string and return to @generateMarkdown
+  else {
     return `[![${appLicense} license](https://img.shields.io/badge/License-${appLicense}-${licenseColor}.svg)](${renderLicenseLink(appLicense)})`
   }
 }
 
+/**
+ * @renderLicenseLink
+ * Returns a string with the license link
+ * to @renderLicenseBadge
+ */
 function renderLicenseLink(appLicense) {
+  // switch the URL based on the license name. If no name, return empty string
   switch(appLicense) {
     case 'MIT':
       return 'https://lbesson.mit-license.org/';
@@ -21,34 +36,72 @@ function renderLicenseLink(appLicense) {
   }
 }
 
+/**
+ * @renderLicenseSection
+ * Returns a string with the license section
+ * to @generateMarkdown
+ */
 function renderLicenseSection(license) {
+  // if no input, return empty string
   if (!license) {
     return "";
-  } else {
+  } 
+  // else, return the formatted string
+  else {
     return `This project is covered under the ${license} license. To learn more, click the license at the top of this page.`
   }
 }
 
+/**
+ * @renderContributingUsers
+ * Splits the string and creates new
+ * array to return to @generateMarkdown
+ */
 function renderContributingUsers (users) {
+  // initialize variables
   let userArr = users.split(/,\s*/),
       newArr = [];
+  // loop through the list of names
   for (let i = 0; i < userArr.length; i++) {
+    // initialize variables
+    // create user github string
     let userLink = `[${userArr[i]}](https://github.com/${userArr[i]})`
+    // push to the newArr
     newArr.push(userLink);
   }
+  // return newArr to @generateMarkdown
   return newArr;
 }
 
+/**
+ * @renderUserEmail
+ * creates email string to return to @generateMarkdown
+ */
 function renderUserEmail (email) {
+  // initialize variables
+  // create email string
   let emailStr = `[${email}](${email})`;
+  // return string to @generateMarkdown
   return emailStr;
 }
 
+/**
+ * @renderUserGithub
+ * creates github user string to return to @generateMarkdown
+ */
 function renderUserGithub (github) {
+  // initialize variables
+  // create github string
   let githubUrl = `https://github.com/${github}`;
+  // return string to @generateMarkdown
   return githubUrl;
 }
 
+/**
+ * @generateMarkdown
+ * Generates the README.md layout to write 
+ * to a new file
+ */
 const generateMarkdown = data => {
   return `
   ${renderLicenseBadge(data)}
